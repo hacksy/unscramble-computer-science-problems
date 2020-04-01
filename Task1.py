3edd818246fc7a2345d1a300b3caa1800c8a3db6
Hansy Schmitt <github@hansyschmitt.com>
2020-04-01 11:45:57 -0500

    Add Solution to Task1

--- Task1.py
+++ Task1.py
@@ -18,3 +18,14 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+def add_only_unique_numbers(previous, phones):
+	for phone in phones:
+		previous.add(phone[0])
+		previous.add(phone[1])
+	return previous
+
+unique_phones = set()
+unique_phones = add_only_unique_numbers(unique_phones, texts)
+unique_phones = add_only_unique_numbers(unique_phones, calls)
+
+print("There are {count} different telephone numbers in the records.".format(count = len(unique_phones)))

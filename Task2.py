ce3d66af74c3b67bbc59bed5e48168134473b5d0
Hansy Schmitt <github@hansyschmitt.com>
2020-04-01 12:27:49 -0500

    Add Solution to Task2

--- Task2.py
+++ Task2.py
@@ -20,3 +20,28 @@ Print a message:
 September 2016.".
 """
 
+from collections import defaultdict
+
+phone_calls = {}
+for call in calls:
+	#Adds total time to sending and receiving phones
+	if call[0]  in phone_calls:
+		phone_calls[call[0]] += int(call[3])
+	else:
+		phone_calls[call[0]] = int(call[3])
+	if call[1]  in phone_calls:
+		phone_calls[call[1]] += int(call[3])
+	else:
+		phone_calls[call[1]] = int(call[3])
+
+max_phone_number = ''
+max_total_time = 0
+for phone in phone_calls:
+	if phone_calls[phone] >= max_total_time:
+		max_phone_number = phone
+		max_total_time = phone_calls[phone]
+print("{telephone_number} spent the longest time, {total_time} seconds, on the phone during September 2016.".format(
+		telephone_number = max_phone_number,
+		total_time = max_total_time
+	)
+)
